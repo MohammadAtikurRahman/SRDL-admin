7af59d93e6a762ff9f3c357de29a0b79d1070e36
Atikur Rahman <46051298+MohammadAtikurRahman@users.noreply.github.com>
2023-06-04 11:42:10 +0600

    new server

--- backend/server.js
+++ backend/server.js
@@ -167,46 +167,44 @@ app.post("/register", async (req, res) => {
 
 
 app.post("/insert-video-data", (req, res) => {
-    const videoData = req.body; // Assuming the videoData is sent in the request body
-  
+    const videoDataArray = req.body; // Assuming the video data array is sent in the request body
+
     // Get the PC name from the system
     const pcName = os.hostname();
-  
-    // Create a new video document using the videoData and PC name
-    const newVideo = {
-      pc_name: pcName,
-      eiin: videoData.eiin,
-      school_name: videoData.school_name,
-      pc_id: videoData.pc_id,
-      lab_id: videoData.lab_id,
-      video_name: videoData.video_name,
-      location: videoData.location,
-      pl_start: videoData.pl_start,
-      start_date_time: videoData.start_date_time,
-      pl_end: videoData.pl_end,
-      end_date_time: videoData.end_date_time,
-      duration: videoData.duration,
-    };
-  
-    // Find the user document by userId and update the pc array with the new video
-    user.findOneAndUpdate(
-      { userId: videoData.userId },
-      { $push: { pc: newVideo } },
-      { new: true }
-    )
-      .then((updatedUser) => {
-        console.log("Data inserted successfully:", updatedUser.pc);
-        res.status(200).json({ message: "Data inserted successfully", pc: updatedUser.pc });
-      })
-      .catch((error) => {
-        console.error("Error inserting data:", error);
-        res.status(500).json({ error: "Error inserting data" });
-      });
-  });
 
+    // Iterate over the video data array and insert each video data into the user document
+    videoDataArray.forEach((videoData) => {
+        const newVideo = {
+            pc_name: pcName,
+            eiin: videoData.eiin,
+            school_name: videoData.school_name,
+            pc_id: videoData.pc_id,
+            lab_id: videoData.lab_id,
+            video_name: videoData.video_name,
+            location: videoData.location,
+            pl_start: videoData.pl_start,
+            start_date_time: videoData.start_date_time,
+            pl_end: videoData.pl_end,
+            end_date_time: videoData.end_date_time,
+            duration: videoData.duration,
+        };
 
+        // Find the user document by userId and update the video array with the new video
+        User.findOneAndUpdate(
+            { userId: videoData.userId },
+            { $push: { video: newVideo } },
+            { new: true }
+        )
+            .then((updatedUser) => {
+                console.log("Data inserted successfully:", updatedUser.video);
+            })
+            .catch((error) => {
+                console.error("Error inserting data:", error);
+            });
+    });
 
-
+    res.status(200).json({ message: "Data insertion started" });
+});
 
 
 
